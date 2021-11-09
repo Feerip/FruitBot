361c946511a6734965571ff8584439139b5046e0
tyler staples <tyler.a.staples@gmail.com>
2021-11-09 12:47:00 -0800

    + Renamed TestSlashCommand.cs to SystemModule + Deleted test button command +Added delete commands command.

---
+++ FruitBot/Modules/SystemModule.cs
@@ -0,0 +1,22 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace FruitBot.Modules
+{
+    public class SystemModule : InteractionModuleBase
+    {
+        [SlashCommand("ping", "recieve a pong")]
+        public async Task Ping() =>
+            await RespondAsync("pong");
+
+#if DEBUG
+        [SlashCommand("delete-commands", "Delete all commands")]
+        public async Task DeleteCommands()
+        {
+            await (Context.Guild as SocketGuild).DeleteApplicationCommandsAsync();
+            await RespondAsync("Guild commands deleted!");
+        }
+#endif
+    }
+}
